4ef25ea3e2590caec6b9ebf12adeb4bc2885b7fc
chhanvirakbuth <nhomreanit.rupp@gmail.com>
2019-06-24 20:24:22 +0700

    commit next

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Requests\UsersRequest;
+use App\User;
+use App\Role;
 
 class AdminUsersController extends Controller
 {
@@ -16,6 +19,8 @@ class AdminUsersController extends Controller
     public function index()
     {
         //
+        $users=User::all();
+        return view('admin.users.index',compact('users'));
         
     }
 
@@ -27,6 +32,8 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
+        $roles=Role::lists('name','id')->all();
+        return view('admin.users.create',compact('roles'));
     }
 
     /**
@@ -35,9 +42,11 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
-        //
+        // User::create($request->all());
+        // return redirect('/admin/users');
+        return $request->all();
     }
 
     /**
